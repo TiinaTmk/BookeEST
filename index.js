1edb5d36772bff45affb3ee2dfd5380d088a5aa4
TiinaTmk <119687726+TiinaTmk@users.noreply.github.com>
2023-11-24 16:38:59 +0200

    BookEst[Delivers #186547860]

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const port = 8080
 const swaggerUI = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json')
 
+
+
+app.get('/rooms', (req, res) => {res.send(["Single", "Suite", "Double"])
+})
+
+
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.listen(port, () => {
   console.log(`Api up at: Http://localhost:${port}`)

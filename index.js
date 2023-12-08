cd5dcc054cbc8f26d37226ed8a9bc902d9d30a0d
TiinaTmk <119687726+TiinaTmk@users.noreply.github.com>
2023-12-08 14:07:55 +0200

    muutused

--- index.js
+++ index.js
@@ -1,28 +1,65 @@
-const app = require('express')()
+const express = require('express');
+const app = express();
 const port = 8080
-const swaggerUI = require('swagger-ui-express')
-const swaggerDocument = require('./docs/swagger.json')
+const swaggerUI = require('swagger-ui-express');
 
+//muuda yaml fail vastavusse apicurioga
+const yamljs = require('yamljs');
+const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 
-const rooms = [
-  
-  {id: 1, name: "Suite", price: 250, description: "This is a suite, our best room in the hotel"},
-  {id: 2, name: "Double", price: 180, description: "This is a double room, which can fit up tp 2 people"},
-  {id: 3, name: "Single", price: 140, description: "This is a single room, which can fit up tp 1 person"},
-  {id: 4, name: "Tripple", price: 180, description: "This is a tripple room, which can fit up tp 3 people"},
-  {id: 5, name: "Junior Suite", price: 230, description: "This is a Junior Suite, our second best room in the hotel"},
-  {id: 6, name: "Family Room", price: 200, description: "This is a family room, which can fit up tp 4 people and is suitable for a family"},
+//const swaggerDocument = require('./docs/swagger.json');
 
+const rooms =[
+    {id:1, name:"Single with mega city view", price: 120.00, decription:"Super cool room with city view, non-smoking" },
+    {id:2, name:"Suite with bath", price: 900.00, decription:"Super cool room with city view and huuge bath, non-smoking"  },
+    {id:3, name:"Double smoking", price: 120.00, decription:"Super cool room with no view at all, smoking allowed"  },
+    {id:4, name:"Double with city view", price: 150.00, decription:"Super cool room with city view, non-smoking"  },
+    {id:5, name:"Suite with bath and city view", price: 1499.99, decription:"Super cool room with city view, non-smoking, bath and shower" },
+    {id:6, name:"Double with city view and bath", price: 250.01, decription:"Super cool double room with city view, non-smoking"  }
 ]
 
-app.get('/rooms', (req, res) => {res.send(rooms)
+app.use(express.json());
+
+app.get('/rooms', (req, res) => {res.send(rooms)});
+
+app.get('/rooms/:id', (req, res) => {
+    if (typeof rooms[req.params.id-1] === 'undefined'){
+    return res.status(404).send({error: "Room not found"})
+    }
+res.send(rooms[req.params.id-1]);
 })
-app.get('/rooms/:id', (req, res) => {res.send(rooms[req.params.id -1])
-});
 
+app.post('/rooms', (req, res) => {
+    if(!req.params.name || !req.params.price || req.params.decription){
+        return res.status(400).send({error: "One or all parameters that are required are missing"})
+    }
+    let room = ({
+        id: rooms.length + 1,
+        name: req.body.name,
+        price: req.body.price,
+        decription: req.body.decription
+    })
+    rooms.push(room)
+    
+    res.status(201)
+    .location(`${getBaseURL(req)}/rooms/${room.length}`)
+    .send(room)
+    });
 
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.delete('/rooms/:id', (req, res) => {
+    if (typeof rooms[req.params.id-1] === 'undefined'){
+        return res.status(404).send({error: "Room not found"})
+    }
+    rooms.splice(req.params.id-1, 1)
+    res.status(204).send({error: "No Content"})
+})
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.listen(port, () => {
-  console.log(`Api up at: Http://localhost:${port}`)
-})
+    console.log(`Api up at: http://localhost:${port}`)});
 
+function getBaseURL(req) {
+    return req.connection && req.connection.encrypted? 
+    'https:' : 'http'+ ` ://${req.headers.host}`;
+}
